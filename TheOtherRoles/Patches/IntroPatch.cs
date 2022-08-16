9c89b72e65b1a4a0fd21d3dbdeb2853fb489ffa3
omega7711 <79310333+omega7711@users.noreply.github.com>
2022-08-17 01:47:27 +0200

    Add files via upload

---
+++ TheOtherRoles/Patches/IntroPatch.cs
@@ -0,0 +1,203 @@
+using HarmonyLib;
+using System;
+using static TheOtherRoles.TheOtherRoles;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using Hazel;
+using TheOtherRoles.Players;
+using TheOtherRoles.Utilities;
+
+namespace TheOtherRoles.Patches {
+    [HarmonyPatch(typeof(IntroCutscene), nameof(IntroCutscene.OnDestroy))]
+    class IntroCutsceneOnDestroyPatch
+    {
+        public static PoolablePlayer playerPrefab;
+        public static void Prefix(IntroCutscene __instance) {
+            // Generate and initialize player icons
+            int playerCounter = 0;
+            if (CachedPlayer.LocalPlayer != null && FastDestroyableSingleton<HudManager>.Instance != null) {
+                Vector3 bottomLeft = new Vector3(-FastDestroyableSingleton<HudManager>.Instance.UseButton.transform.localPosition.x, FastDestroyableSingleton<HudManager>.Instance.UseButton.transform.localPosition.y, FastDestroyableSingleton<HudManager>.Instance.UseButton.transform.localPosition.z);
+                foreach (PlayerControl p in CachedPlayer.AllPlayers) {
+                    GameData.PlayerInfo data = p.Data;
+                    PoolablePlayer player = UnityEngine.Object.Instantiate<PoolablePlayer>(__instance.PlayerPrefab, FastDestroyableSingleton<HudManager>.Instance.transform);
+                    playerPrefab = __instance.PlayerPrefab;
+                    p.SetPlayerMaterialColors(player.cosmetics.currentBodySprite.BodySprite);
+                    player.SetSkin(data.DefaultOutfit.SkinId, data.DefaultOutfit.ColorId);
+                    player.cosmetics.SetHat(data.DefaultOutfit.HatId, data.DefaultOutfit.ColorId);
+                   // PlayerControl.SetPetImage(data.DefaultOutfit.PetId, data.DefaultOutfit.ColorId, player.PetSlot);
+                    player.cosmetics.nameText.text = data.PlayerName;
+                    player.SetFlipX(true);
+                    MapOptions.playerIcons[p.PlayerId] = player;
+
+                    if (CachedPlayer.LocalPlayer.PlayerControl == Arsonist.arsonist && p != Arsonist.arsonist) {
+                        player.transform.localPosition = bottomLeft + new Vector3(-0.25f, -0.25f, 0) + Vector3.right * playerCounter++ * 0.35f;
+                        player.transform.localScale = Vector3.one * 0.2f;
+                        player.setSemiTransparent(true);
+                        player.gameObject.SetActive(true);
+                    } else if (CachedPlayer.LocalPlayer.PlayerControl == BountyHunter.bountyHunter) {
+                        player.transform.localPosition = bottomLeft + new Vector3(-0.25f, 0f, 0);
+                        player.transform.localScale = Vector3.one * 0.4f;
+                        player.gameObject.SetActive(false);
+                    } else {
+                        player.gameObject.SetActive(false);
+                    }
+                }
+            }
+
+            // Force Bounty Hunter to load a new Bounty when the Intro is over
+            if (BountyHunter.bounty != null && CachedPlayer.LocalPlayer.PlayerControl == BountyHunter.bountyHunter) {
+                BountyHunter.bountyUpdateTimer = 0f;
+                if (FastDestroyableSingleton<HudManager>.Instance != null) {
+                    Vector3 bottomLeft = new Vector3(-FastDestroyableSingleton<HudManager>.Instance.UseButton.transform.localPosition.x, FastDestroyableSingleton<HudManager>.Instance.UseButton.transform.localPosition.y, FastDestroyableSingleton<HudManager>.Instance.UseButton.transform.localPosition.z) + new Vector3(-0.25f, 1f, 0);
+                    BountyHunter.cooldownText = UnityEngine.Object.Instantiate<TMPro.TextMeshPro>(FastDestroyableSingleton<HudManager>.Instance.KillButton.cooldownTimerText, FastDestroyableSingleton<HudManager>.Instance.transform);
+                    BountyHunter.cooldownText.alignment = TMPro.TextAlignmentOptions.Center;
+                    BountyHunter.cooldownText.transform.localPosition = bottomLeft + new Vector3(0f, -1f, -1f);
+                    BountyHunter.cooldownText.gameObject.SetActive(true);
+                }
+            }
+
+            // Force Reload of SoundEffectHolder
+            SoundEffectsManager.Load();
+
+            // First kill
+            if (AmongUsClient.Instance.AmHost && MapOptions.shieldFirstKill && MapOptions.firstKillName != "") {
+                PlayerControl target = PlayerControl.AllPlayerControls.ToArray().ToList().FirstOrDefault(x => x.Data.PlayerName.Equals(MapOptions.firstKillName));
+                if (target != null) {
+                    MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(CachedPlayer.LocalPlayer.PlayerControl.NetId, (byte)CustomRPC.SetFirstKill, Hazel.SendOption.Reliable, -1);
+                    writer.Write(target.PlayerId);
+                    AmongUsClient.Instance.FinishRpcImmediately(writer);
+                    RPCProcedure.setFirstKill(target.PlayerId);
+                }
+            }
+            MapOptions.firstKillName = "";
+        }
+    }
+
+    [HarmonyPatch]
+    class IntroPatch {
+        public static void setupIntroTeamIcons(IntroCutscene __instance, ref  Il2CppSystem.Collections.Generic.List<PlayerControl> yourTeam) {
+            // Intro solo teams
+            if (CachedPlayer.LocalPlayer.PlayerControl == Jester.jester || CachedPlayer.LocalPlayer.PlayerControl == Jackal.jackal || CachedPlayer.LocalPlayer.PlayerControl == Arsonist.arsonist || CachedPlayer.LocalPlayer.PlayerControl == Vulture.vulture || CachedPlayer.LocalPlayer.PlayerControl == Lawyer.lawyer) {
+                var soloTeam = new Il2CppSystem.Collections.Generic.List<PlayerControl>();
+                soloTeam.Add(CachedPlayer.LocalPlayer.PlayerControl);
+                yourTeam = soloTeam;
+            }
+
+            // Add the Spy to the Impostor team (for the Impostors)
+            if (Spy.spy != null && CachedPlayer.LocalPlayer.Data.Role.IsImpostor) {
+                List<PlayerControl> players = PlayerControl.AllPlayerControls.ToArray().ToList().OrderBy(x => Guid.NewGuid()).ToList();
+                var fakeImpostorTeam = new Il2CppSystem.Collections.Generic.List<PlayerControl>(); // The local player always has to be the first one in the list (to be displayed in the center)
+                fakeImpostorTeam.Add(CachedPlayer.LocalPlayer.PlayerControl);
+                foreach (PlayerControl p in players) {
+                    if (CachedPlayer.LocalPlayer.PlayerControl != p && (p == Spy.spy || p.Data.Role.IsImpostor))
+                        fakeImpostorTeam.Add(p);
+                }
+                yourTeam = fakeImpostorTeam;
+            }
+        }
+
+        public static void setupIntroTeam(IntroCutscene __instance, ref  Il2CppSystem.Collections.Generic.List<PlayerControl> yourTeam) {
+            List<RoleInfo> infos = RoleInfo.getRoleInfoForPlayer(CachedPlayer.LocalPlayer.PlayerControl);
+            RoleInfo roleInfo = infos.Where(info => !info.isModifier).FirstOrDefault();
+            if (roleInfo == null) return;
+            if (roleInfo.isNeutral) {
+                var neutralColor = new Color32(76, 84, 78, 255);
+                __instance.BackgroundBar.material.color = neutralColor;
+                __instance.TeamTitle.text = "Neutre";
+                __instance.TeamTitle.color = neutralColor;
+            }
+        }
+
+        public static IEnumerator<WaitForSeconds> EndShowRole(IntroCutscene __instance) {
+            yield return new WaitForSeconds(5f);
+            __instance.YouAreText.gameObject.SetActive(false);
+            __instance.RoleText.gameObject.SetActive(false);
+            __instance.RoleBlurbText.gameObject.SetActive(false);
+            __instance.ourCrewmate.gameObject.SetActive(false);
+           
+        }
+
+        [HarmonyPatch(typeof(IntroCutscene), nameof(IntroCutscene.CreatePlayer))]
+        class CreatePlayerPatch {
+            public static void Postfix(IntroCutscene __instance, bool impostorPositioning, ref PoolablePlayer __result) {
+                if (impostorPositioning) __result.SetNameColor(Palette.ImpostorRed);
+            }
+        }
+
+
+        [HarmonyPatch(typeof(IntroCutscene), nameof(IntroCutscene.ShowRole))]
+        class SetUpRoleTextPatch {
+            static public void SetRoleTexts(IntroCutscene __instance) {
+                // Don't override the intro of the vanilla roles
+                List<RoleInfo> infos = RoleInfo.getRoleInfoForPlayer(CachedPlayer.LocalPlayer.PlayerControl);
+                RoleInfo roleInfo = infos.Where(info => !info.isModifier).FirstOrDefault();
+                RoleInfo modifierInfo = infos.Where(info => info.isModifier).FirstOrDefault();
+                __instance.RoleBlurbText.text = "";
+                if (roleInfo != null) {
+                    __instance.RoleText.text = roleInfo.name;
+                    __instance.RoleText.color = roleInfo.color;
+                    __instance.RoleBlurbText.text = roleInfo.introDescription;
+                    __instance.RoleBlurbText.color = roleInfo.color;
+                }
+                if (modifierInfo != null) {
+                    if (modifierInfo.roleId != RoleId.Lover)
+                        __instance.RoleBlurbText.text += Helpers.cs(modifierInfo.color, $"\n{modifierInfo.introDescription}");
+                    else {
+                        PlayerControl otherLover = CachedPlayer.LocalPlayer.PlayerControl == Lovers.lover1 ? Lovers.lover2 : Lovers.lover1;
+                        __instance.RoleBlurbText.text += Helpers.cs(Lovers.color, $"\n♥ Vous êtes en couple avec {otherLover?.Data?.PlayerName ?? ""} ♥");
+                    }
+                }
+                if (Deputy.knowsSheriff && Deputy.deputy != null && Sheriff.sheriff != null) {
+                    if (infos.Any(info => info.roleId == RoleId.Sheriff))
+                        __instance.RoleBlurbText.text += Helpers.cs(Sheriff.color, $"\nVotre Adjoint est {Deputy.deputy?.Data?.PlayerName ?? ""}");
+                    else if (infos.Any(info => info.roleId == RoleId.Deputy))
+                        __instance.RoleBlurbText.text += Helpers.cs(Sheriff.color, $"\nVotre Shérif est {Sheriff.sheriff?.Data?.PlayerName ?? ""}");
+                }
+            }
+            public static bool Prefix(IntroCutscene __instance) {
+                if (!CustomOptionHolder.activateRoles.getBool()) return true;
+                FastDestroyableSingleton<HudManager>.Instance.StartCoroutine(Effects.Lerp(1f, new Action<float>((p) => {
+                    SetRoleTexts(__instance);
+                })));
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(IntroCutscene), nameof(IntroCutscene.BeginCrewmate))]
+        class BeginCrewmatePatch {
+            public static void Prefix(IntroCutscene __instance, ref  Il2CppSystem.Collections.Generic.List<PlayerControl> teamToDisplay) {
+                setupIntroTeamIcons(__instance, ref teamToDisplay);
+            }
+
+            public static void Postfix(IntroCutscene __instance, ref  Il2CppSystem.Collections.Generic.List<PlayerControl> teamToDisplay) {
+                setupIntroTeam(__instance, ref teamToDisplay);
+            }
+        }
+
+        [HarmonyPatch(typeof(IntroCutscene), nameof(IntroCutscene.BeginImpostor))]
+        class BeginImpostorPatch {
+            public static void Prefix(IntroCutscene __instance, ref  Il2CppSystem.Collections.Generic.List<PlayerControl> yourTeam) {
+                setupIntroTeamIcons(__instance, ref yourTeam);
+            }
+
+            public static void Postfix(IntroCutscene __instance, ref  Il2CppSystem.Collections.Generic.List<PlayerControl> yourTeam) {
+                setupIntroTeam(__instance, ref yourTeam);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Constants), nameof(Constants.ShouldHorseAround))]
+    public static class ShouldAlwaysHorseAround {
+        public static bool isHorseMode;
+        public static bool Prefix(ref bool __result) {
+            if (isHorseMode != MapOptions.enableHorseMode && LobbyBehaviour.Instance != null) __result = isHorseMode;
+            else {
+                __result = MapOptions.enableHorseMode;
+                isHorseMode = MapOptions.enableHorseMode;
+            }
+            return false;
+        }
+    }
+}
+
---
+++ TheOtherRoles/Patches/MainMenuPatch.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.UI.Button;
+using Object = UnityEngine.Object;
+using TheOtherRoles.Patches;
+
+namespace TheOtherRoles.Modules {
+    [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+    public class MainMenuPatch {
+        private static bool horseButtonState = MapOptions.enableHorseMode;
+        private static Sprite horseModeOffSprite = null;
+        private static Sprite horseModeOnSprite = null;
+        private static GameObject bottomTemplate;
+        private static AnnouncementPopUp popUp;
+
+        private static void Prefix(MainMenuManager __instance) {
+            CustomHatLoader.LaunchHatFetcher();
+            var template = GameObject.Find("ExitGameButton");
+            if (template == null) return;
+
+            var buttonDiscord = UnityEngine.Object.Instantiate(template, null);
+            buttonDiscord.transform.localPosition = new Vector3(buttonDiscord.transform.localPosition.x, buttonDiscord.transform.localPosition.y + 0.6f, buttonDiscord.transform.localPosition.z);
+
+            var textDiscord = buttonDiscord.transform.GetChild(0).GetComponent<TMPro.TMP_Text>();
+            __instance.StartCoroutine(Effects.Lerp(0.1f, new System.Action<float>((p) => {
+                textDiscord.SetText("Discord");
+            })));
+
+            PassiveButton passiveButtonDiscord = buttonDiscord.GetComponent<PassiveButton>();
+            SpriteRenderer buttonSpriteDiscord = buttonDiscord.GetComponent<SpriteRenderer>();
+
+            passiveButtonDiscord.OnClick = new Button.ButtonClickedEvent();
+            passiveButtonDiscord.OnClick.AddListener((System.Action)(() => Application.OpenURL("https://discord.gg/77RkMJHWsM")));
+
+            Color discordColor = new Color32(88, 101, 242, byte.MaxValue);
+            buttonSpriteDiscord.color = textDiscord.color = discordColor;
+            passiveButtonDiscord.OnMouseOut.AddListener((System.Action)delegate {
+                buttonSpriteDiscord.color = textDiscord.color = discordColor;
+            });
+
+
+            // Horse mode stuff
+            var horseModeSelectionBehavior = new ClientOptionsPatch.SelectionBehaviour("Enable Horse Mode", () => MapOptions.enableHorseMode = TheOtherRolesPlugin.EnableHorseMode.Value = !TheOtherRolesPlugin.EnableHorseMode.Value, TheOtherRolesPlugin.EnableHorseMode.Value);
+
+            bottomTemplate = GameObject.Find("InventoryButton");
+            if (bottomTemplate == null) return;
+            var horseButton = Object.Instantiate(bottomTemplate, bottomTemplate.transform.parent);
+            var passiveHorseButton = horseButton.GetComponent<PassiveButton>();
+            var spriteHorseButton = horseButton.GetComponent<SpriteRenderer>();
+
+            horseModeOffSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOff.png", 75f);
+            horseModeOnSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOn.png", 75f);
+
+            spriteHorseButton.sprite = horseButtonState ? horseModeOnSprite : horseModeOffSprite;
+
+            passiveHorseButton.OnClick = new ButtonClickedEvent();
+
+            passiveHorseButton.OnClick.AddListener((System.Action)delegate {
+                horseButtonState = horseModeSelectionBehavior.OnClick();
+                if (horseButtonState) {
+                    if (horseModeOnSprite == null) horseModeOnSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOn.png", 75f);
+                    spriteHorseButton.sprite = horseModeOnSprite;
+                } else {
+                    if (horseModeOffSprite == null) horseModeOffSprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.HorseModeButtonOff.png", 75f);
+                    spriteHorseButton.sprite = horseModeOffSprite;
+                }
+                CredentialsPatch.LogoPatch.updateSprite();
+                // Avoid wrong Player Particles floating around in the background
+                var particles = GameObject.FindObjectOfType<PlayerParticles>();
+                if (particles != null) {
+                    particles.pool.ReclaimAll();
+                    particles.Start();
+                }
+            });
+
+            // TOR credits button
+            if (bottomTemplate == null) return;
+            var creditsButton = Object.Instantiate(bottomTemplate, bottomTemplate.transform.parent);
+            var passiveCreditsButton = creditsButton.GetComponent<PassiveButton>();
+            var spriteCreditsButton = creditsButton.GetComponent<SpriteRenderer>();
+
+            spriteCreditsButton.sprite = Helpers.loadSpriteFromResources("TheOtherRoles.Resources.CreditsButton.png", 75f);
+
+            passiveCreditsButton.OnClick = new ButtonClickedEvent();
+
+            passiveCreditsButton.OnClick.AddListener((System.Action)delegate {
+                // do stuff
+                if (popUp != null) Object.Destroy(popUp);
+                popUp = Object.Instantiate(Object.FindObjectOfType<AnnouncementPopUp>(true));
+                popUp.gameObject.SetActive(true);
+                popUp.Init();
+                //SelectableHyperLinkHelper.DestroyGOs(popUp.selectableHyperLinks, "test");
+                string creditsString = @$"<align=""center"">Contributeurs GitHub:
+Gendelo
+Alex2911    amsyarasyiq    MaximeGillot
+Psynomit    probablyadnf    JustASysAdmin
+Traduction:
+omega7711    Shadhow   AKONE
+Cahty_12
+
+";
+                creditsString += $@"<size=60%> Autres crédits et ressources:
+OxygenFilter - pour toutes les versions allant de v2.3.0 à v2.6.1, on utilisait OxygenFilter pour un désobscurcissement automatique
+Reactor - le framework utilisé pour toutes les versions avant la v2.0.0
+BepInEx - Utilisé pour hook les fonctions de jeu
+Essentials - Options de jeu custom par DorCoMaNdO:
+
+Avant v1.6: On utilisait la version par défaut d'Essentials
+v1.6-v1.8: On a légèrement changé la version par défaut d'Essentials. Les changements peuvent être trouvés dans la branche de notre fork.
+v2.0.0 et après later: Comme on n'utilise plus Reactor, on utilise notre propre implémentation, inspiré de celle de DorCoMaNdO
+Jackal and Sidekick - Idée originale du Jackal et du Sidekick vanant de Dhalucard
+Among-Us-Love-Couple-Mod - Idée des rôles Lovers venant de Woodi-dev
+Jester - Idée du rôle Jester venant de Maartii
+ExtraRolesAmongUs - Idée des rôles Engineer et Medic venant de NotHunter101. De plus, certains extraits de code issus de l'implémentation ont été utilisés.
+Among-Us-Sheriff-Mod - Idée du rôle Sheriff venant de Woodi-dev
+TooManyRolesMods - Idée des rôles Detective et Time Master venant de Hardel-DW. De plus, certains extraits de code issus de l'implémentation ont été utilisés.
+TownOfUs - Idée des rôles Swapper, Shifter, Arsonist et similaire au Mayor venant de Slushiegoose
+Ottomated - Idée des rôles Morphling, Snitch et Camouflager venant de Ottomated
+Crowded-Mod - Notre implémentation pour les lobby de 10+ joueurs est inspiré de celle de venant de la Crowded Mod Team
+Goose-Goose-Duck - Idée pour le rôle Vulture venant de Slushygoose</size>";
+                creditsString += "</align>";
+                popUp.AnnounceTextMeshPro.text = creditsString;
+                __instance.StartCoroutine(Effects.Lerp(0.01f, new Action<float>((p) => {
+                    if (p == 1) {
+                        var titleText = GameObject.Find("Title_Text").GetComponent<TMPro.TextMeshPro>();
+                        if (titleText != null) titleText.text = "Crédits and Contributeurs";
+                    }
+                })));
+            });
+            
+        }
+
+        public static void Postfix(MainMenuManager __instance) {
+            __instance.StartCoroutine(Effects.Lerp(0.01f, new Action<float>((p) => {
+                if (p == 1) {
+                    bottomTemplate = GameObject.Find("InventoryButton");
+                    foreach (Transform tf in bottomTemplate.transform.parent.GetComponentsInChildren<Transform>()) {
+                        tf.localPosition = new Vector2(tf.localPosition.x * 0.8f, tf.localPosition.y);
+                    }
+                }
+            })));
+
+        }
+    }
+
+    [HarmonyPatch(typeof(AnnouncementPopUp), nameof(AnnouncementPopUp.UpdateAnnounceText))]
+    public static class Announcement
+    {
+        public static ModUpdateBehaviour.UpdateData updateData = null;
+        public static bool Prefix(AnnouncementPopUp __instance)
+        {
+            if (ModUpdateBehaviour.showPopUp || updateData == null) return true;
+
+            var text = __instance.AnnounceTextMeshPro;            
+            text.text = $"<size=150%><color=#FC0303>THE OTHER ROLES FR</color> {(updateData.Tag)}\n{(updateData.Content)}";
+
+            return false;
+        }
+    }
+}
